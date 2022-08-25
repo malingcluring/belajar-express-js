864161b609f281d9b4b63f54b89cb262e3d99112
malingcluring <maling.maling.maling.maling@gmail.com>
2022-08-25 10:26:46 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
 const app = express();
+app.engine('hbs', engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './views'));
 
 // port 3030
 const port = 3030;
 
 // route
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
